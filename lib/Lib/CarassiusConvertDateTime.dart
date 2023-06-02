278484a63ce66ceaea09dc6af73f0e191eb0defd
Bobby Anggunawan <yeremia.blog@gmail.com>
2023-06-02 11:46:09 +0700

    tambah fungsi toStringJustTime() ke CarassiusConvertDateTime

--- lib/Lib/CarassiusConvertDateTime.dart
+++ lib/Lib/CarassiusConvertDateTime.dart
@@ -12,9 +12,26 @@ class CarassiusConvertDateTime{
 
   /// ubah datetime ke string dengan format dd-mm-yyyy. misalnya 21-04-2000
   ///
-  /// **Note**, karakter separator default adalah "-". Karakter ini bisa diganti dengan menset parameter [separator]
-  static String toStringJustDateAllNumber(DateTime? data, {String seperator = "-"}){
-    if(data == null) return "-";
+  /// **Note**
+  /// * karakter separator default adalah "-". Karakter ini bisa diganti dengan menset parameter [separator]
+  /// * kalau data == null, fungsi ini akan mengembalikan string yang di set di parameter [showIfNull].
+  static String toStringJustDateAllNumber(DateTime? data, {String seperator = "-", String showIfNull = "-"}){
+    if(data == null) return showIfNull;
     return "${CarassiusConvertInt.toStringTwoDigitNumber(data.day)}${seperator}${CarassiusConvertInt.toStringTwoDigitNumber(data.month)}${seperator}${data.year}";
+  }
+
+  /// ubah datetime ke string dengan format jj:mm. misalnya 07:50
+  ///
+  /// **Note**
+  /// * karakter separator default adalah ":". Karakter ini bisa diganti dengan menset parameter [separator]
+  /// * kalau data == null, fungsi ini akan mengembalikan string yang di set di parameter [showIfNull].
+  /// * kalau [showWithSecond] == true, juga akan menampilkan detik, kalau false hanya menampilkan jam dan menit
+  static String toStringJustTime(DateTime? data, bool showWithSecond, {String seperator = "-", String showIfNull = "-"}){
+    if(data == null) return showIfNull;
+
+    if(showWithSecond){
+      return "${CarassiusConvertInt.toStringTwoDigitNumber(data.hour)}${seperator}${CarassiusConvertInt.toStringTwoDigitNumber(data.minute)}${seperator}${CarassiusConvertInt.toStringTwoDigitNumber(data.second)}";
+    }
+    return "${CarassiusConvertInt.toStringTwoDigitNumber(data.hour)}${seperator}${CarassiusConvertInt.toStringTwoDigitNumber(data.minute)}";
   }
 }
